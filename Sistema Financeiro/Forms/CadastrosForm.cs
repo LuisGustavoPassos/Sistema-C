a44cc0ba4f52352a9dfe0e57d7ec23c71095b317
Luis Gustavo <86069439+LuisGustavoPassos@users.noreply.github.com>
2025-04-03 16:48:02 -0300

    Já da pra chamar de versão 0.1
    o projeto está com base agora dá começar a fazer as funções

---
+++ Sistema Financeiro/Forms/CadastrosForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Financeiro
+{
+    public partial class CadastrosForm : Form
+    {
+        public CadastrosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CadastrosForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
